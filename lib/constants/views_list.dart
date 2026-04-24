8ea268fce98a455130c1e12542cdf52d0f83d238
Salah <salahandrea99@gmail.com>
2026-04-24 09:07:33 +0200

    added tabs

--- lib/constants/views_list.dart
+++ lib/constants/views_list.dart
@@ -2,4 +2,4 @@ import 'package:movie_app/views/home/home_view.dart';
 import 'package:movie_app/views/profile/profile_view.dart';
 import 'package:movie_app/views/search/search_view.dart';
 
-final List Views = const [HomeView(), SearchView(), ProfileView()];
+List<dynamic> views = const [HomeView(), SearchView(), ProfileView()];
--- lib/layout/main_layout.dart
+++ lib/layout/main_layout.dart
@@ -1,10 +1,38 @@
+import 'package:movie_app/constants/views_list.dart';
+
 import 'package:flutter/material.dart';
 
-class MyWidget extends StatelessWidget {
-  const MyWidget({super.key});
+class MainLayout extends StatefulWidget {
+  const MainLayout({super.key});
 
   @override
+  State<MainLayout> createState() => _MainLayoutState();
+}
+
+class _MainLayoutState extends State<MainLayout> {
+  int currentIndex = 0;
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: views[currentIndex],
+      bottomNavigationBar: NavigationBar(
+        indicatorColor: Color.fromARGB(255, 161, 175, 179),
+        selectedIndex: currentIndex,
+        // animationDuration: Duration(milliseconds: 150),
+        onDestinationSelected: (selcetedIndex) {
+          setState(() {
+            currentIndex = selcetedIndex;
+          });
+        },
+        destinations: const [
+          NavigationDestination(icon: Icon(Icons.home), label: "Home"),
+          NavigationDestination(icon: Icon(Icons.search), label: "Search"),
+          NavigationDestination(
+            icon: Icon(Icons.person_outline),
+            label: "Profile",
+          ),
+        ],
+      ),
+    );
   }
 }
